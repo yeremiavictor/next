6b5c36a0d23d3773297b4fb4c3cea59462c29424
yeremiavictor <yeremiavictor@gmail.com>
2022-10-17 14:21:20 +0700

    deleted:    pages/blog/[blogid]/coment/[comentid].js 	deleted:    pages/blog/[blogid]/index.js 	deleted:    pages/blog/add.js 	deleted:    pages/blog/category.js 	deleted:    pages/blog/index.js 	deleted:    pages/post/[...slug].js 	new file:   pages/product/index.js

--- pages/blog/category.js
+++
@@ -1,7 +0,0 @@
-const category = () => {
-  return (
-    <div><h2>This is Category</h2></div>
-  )
-}
-
-export default category
---
+++ pages/product/index.js
@@ -0,0 +1,7 @@
+const Products = () => {
+  return (
+    <div>Products List</div>
+  )
+}
+
+export default Products
